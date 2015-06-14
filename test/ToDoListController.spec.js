648a743eeef8f0de6027bb16d9a85176bf805f94
cmanessis <cmpetrowii@gmail.com>
2015-06-15 00:49:29 +0100

    no test,just a prototype todo list

---
+++ test/ToDoListController.spec.js
@@ -0,0 +1,9 @@
+describe('ToDolistController', function() {
+  beforeEach(module('ToDoList'));
+
+  var ctrl;
+
+  beforeEach(inject(function($controller) {
+    ctrl = $controller('ToDolistController');
+  }));
+});
